a12d271f1f8cea7810a489aa6fbc39f26a4b064d
cullenpeterson <c.m.peterson@wustl.edu>
2024-06-17 13:41:20 -0500

    divided into 2 files

--- calculator.php
+++ calculator.php
@@ -8,26 +8,6 @@
 </head>
 <body>
 <h1>CALCULATOR</h1>
-<form method="get" action="">
-        <label for="number1">Number 1:</label>
-        <input type="number" name="number1" id="number1" step="any" required>
-        <br><br>
-        <label for="number2">Number 2:</label>
-        <input type="number" name="number2" id="number2" step="any" required>
-        <br><br>
-        <label>Operation:</label>
-        <input type="radio" name="operation" id="add" value="add" required>
-        <label for="add">Addition</label>
-        <input type="radio" name="operation" id="subtract" value="subtract" required>
-        <label for="subtract">Subtraction</label>
-        <input type="radio" name="operation" id="multiply" value="multiply" required>
-        <label for="multiply">Multiplication</label>
-        <input type="radio" name="operation" id="divide" value="divide" required>
-        <label for="divide">Division</label>
-        <br><br>
-        <input type="submit" value="Calculate">
-    </form>
-
 <?php
     $num1 = floatval($_GET["number1"]);
     $num2 = floatval($_GET["number2"]);
@@ -55,7 +35,7 @@
             break;
     }
     //output result here
-    echo "<h2>Result: $result</h2>";
+    echo "Result: $result";
 
 ?>
     
